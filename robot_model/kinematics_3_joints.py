46d4efc9df2da6a6f9e3c4c3d038f748ccc71fe2
Nicolas Wim Landuyt <wlanduyt@tudelft.nl>
2023-12-13 12:51:16 +0100

    file cleanup

--- robot_model/kinematics_3_joints.py
+++ robot_model/kinematics_3_joints.py
@@ -1,126 +1,30 @@
-# import numpy as np
-# import warnings
-
-# class Kinematics3joints:
-#     def __init__(self, joint_angles):
-#         # Throw error when wrong amount of angles is provided
-#         if len(joint_angles) != 3:
-#             raise ValueError("The wrong amount of angles was provided.")
-        
-#         # Update joint angle limits and speed limits for three joints
-#         self.angle_limits = [
-#             (-2.8973, 2.8973),
-#             (-1.7628, 1.7628),
-#             (-2.8973, 2.8973)
-#         ]
-#         self.speed_limits = [
-#             (2.1750,),
-#             (2.1750,),
-#             (2.1750,)
-#         ]
-        
-#         # Ensure joint angles are within limits
-#         for i, (angle, (min, max)) in enumerate(zip(joint_angles, self.angle_limits)):
-#             if not (min <= angle <= max):
-#                 corrected_angle = np.clip(angle, min, max)
-#                 warnings.warn(f"Joint angle {angle} for joint {i+1} is out of limits: {min} to {max}. Setting to {corrected_angle}.")
-#                 joint_angles[i] = corrected_angle
-        
-#         # Update DH parameters for the first three joints only
-#         self.dh_parameters = [
-#             [0, 0, 0.333, joint_angles[0]],
-#             [0, -np.pi/2, 0, joint_angles[1]],
-#             [0, np.pi/2, 0.316, joint_angles[2]]
-#         ]
-
-    
-#     def transformation_matrix(self, a, alpha, d, theta):
-#         T = np.array([  
-#             [np.cos(theta), -np.sin(theta)*np.cos(alpha), np.sin(theta)*np.sin(alpha), a*np.cos(theta)],
-#             [np.sin(theta), np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha), a*np.sin(theta)],
-#             [0, np.sin(alpha), np.cos(alpha), d],
-#             [0, 0, 0, 1]])
-#         return T
-
-#     def forward_kinematics(self):
-#         T = np.eye(4)
-#         for parameters in self.dh_parameters:
-#             a = parameters[0]
-#             alpha = parameters[1]
-#             d = parameters[2]
-#             theta = parameters[3]
-
-#             Ti = self.transformation_matrix(a, alpha, d, theta)
-#             T = np.dot(T, Ti)
-
-#         x, y, z = T[0, 3], T[1, 3], T[2, 3]
-
-#         return x, y, z
-
-#     def jacobian(self):
-#         numjoints = len(self.dh_parameters)
-#         J = np.zeros((6, numjoints))
-
-#         T_all = [np.eye(4)]
-
-#         # Calculate transforms for the first three joints
-#         for i in range(numjoints):
-#             a, alpha, d, theta = self.dh_parameters[i]
-#             T = self.transformation_matrix(a, alpha, d, theta)
-#             T_0_i = np.matmul(T_all[-1], T)
-#             T_all.append(T_0_i)
-
-#         T_0_end = T_all[-1]
-#         t_end = T_0_end[0:3, 3]
-
-#         # Build Jacobian for the first three joints
-#         for i in range(numjoints):
-#             T_0_i = T_all[i]
-#             z_i = T_0_i[0:3, 2]
-#             t_i = T_0_i[0:3, 3]
-
-#             J[0:3, i] = np.round(np.cross(z_i, (t_end - t_i)), 3)
-#             J[3:6, i] = np.round(z_i, 3)
-
-#         return J
-
-# # Usage example
-# # joint_angles = [0, 0, 0]
-# # kinematics = Kinematics(joint_angles)
-# # position = kinematics.forward_kinematics()
-# # print(f"The end effector position is: {position}")
-# # jacobian_matrix = kinematics.jacobian()
-# # print("Jacobian Matrix:")
-# # print(jacobian_matrix)
-
 import numpy as np
 import warnings
+from sympy import symbols, cos, sin, Matrix, lambdify, pi
+
 
 class Kinematics3joints:
-    def __init__(self, joint_angles):
-        # Validate the number of joint angles provided
+    def __init__(self, joint_angles, base_transformation=np.eye(4)):
         if len(joint_angles) != 3:
             raise ValueError("The wrong amount of angles was provided.")
         
-        # Define joint angle limits for the three joints
         self.angle_limits = [(-2.8973, 2.8973), (-1.7628, 1.7628), (-2.8973, 2.8973)]
         
-        # Ensure joint angles are within limits
         for i, (angle, (min_angle, max_angle)) in enumerate(zip(joint_angles, self.angle_limits)):
             if not (min_angle <= angle <= max_angle):
                 corrected_angle = np.clip(angle, min_angle, max_angle)
                 warnings.warn(f"Joint angle {angle} for joint {i+1} is out of limits: {min_angle} to {max_angle}. Setting to {corrected_angle}.")
                 joint_angles[i] = corrected_angle
 
-        # Define DH parameters for the first three joints
         self.dh_parameters = [
             [0, 0, 0.333, joint_angles[0]],
             [0, -np.pi/2, 0, joint_angles[1]],
             [0, np.pi/2, 0.316, joint_angles[2]]
-        ]
 
-    def dh_transform(self, alpha, a, d, theta):
-        # Calculate the DH transformation matrix
+        ]
+        self.base_transformation = base_transformation
+
+    def dh_transform(self, a, alpha, d, theta):
         T = np.array([
             [np.cos(theta), -np.sin(theta)*np.cos(alpha), np.sin(theta)*np.sin(alpha), a*np.cos(theta)],
             [np.sin(theta), np.cos(theta)*np.cos(alpha), -np.cos(theta)*np.sin(alpha), a*np.sin(theta)],
@@ -128,19 +32,44 @@ class Kinematics3joints:
             [0, 0, 0, 1]
         ])
         return T
+    
+    def calculate_transformation_matrix(self, a, alpha, d, theta):
+        ca = np.cos(alpha)
+        sa = np.sin(alpha)
+        cq = np.cos(theta)
+        sq = np.sin(theta)
 
+        # Define transformation matrix for the current joint
+        transform = Matrix([
+            [cq, -sq, 0, a],
+            [ca * sq, ca * cq, -sa, -d * sa],
+            [sa * sq, cq * sa, ca, d * ca],
+            [0, 0, 0, 1]
+        ])
+
+        return transform
+
+    # def forward_kinematics(self):
+    #     T_all = [self.base_transformation]
+    #     for a, alpha, d, theta in self.dh_parameters:
+    #         T = self.dh_transform(a, alpha, d, theta)
+    #         T_0_i = np.dot(T_all[-1], T)
+    #         T_all.append(T_0_i)
+
+    #     return T_all[-1]
+    
     def forward_kinematics(self):
-        # Calculate forward kinematics
-        T = np.eye(4)
-        for alpha, a, d, theta in self.dh_parameters:
-            Ti = self.dh_transform(alpha, a, d, theta)
-            T = np.dot(T, Ti)
+        # Initialize the transformation matrix
+        T_total = self.base_transformation
 
-        x, y, z = T[0, 3], T[1, 3], T[2, 3]
-        return x, y, z
+        # Iterate over the three joints
+        for dh_params in self.dh_parameters:
+            a, alpha, d, theta = dh_params
 
-# Usage example
-# joint_angles = [0, 0, 0]
-# kinematics = Kinematics3joints(joint_angles)
-# position = kinematics.forward_kinematics()
-# print(f"The end effector position is: {position}")
+            # Calculate the transformation matrix for the current joint
+            T_joint = self.calculate_transformation_matrix(a, alpha, d, theta)
+
+            # Multiply the total transformation matrix by the current joint's transformation
+            T_total = np.dot(T_total, T_joint)
+
+        return T_total
--- robot_model/operate_arm_3_joints.py
+++ robot_model/operate_arm_3_joints.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import warnings
 import gymnasium as gym
 import numpy as np
@@ -6,10 +5,10 @@ from urdfenvs.robots.generic_urdf.generic_diff_drive_robot import GenericDiffDri
 from urdfenvs.urdf_common.urdf_env import UrdfEnv
 import pybullet as p
 from kinematics_3_joints import Kinematics3joints
+import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def run_albert(n_steps=1000, render=False, goal=True, obstacles=True, albert_radius=0.3):
-    # Environment setup
+def run_albert(n_steps=100, render=False, goal=True, obstacles=True, albert_radius=0.3):
     robots = [
         GenericDiffDriveRobot(
             urdf="albert.urdf",
@@ -23,53 +22,73 @@ def run_albert(n_steps=1000, render=False, goal=True, obstacles=True, albert_rad
         ),
     ]
     env = gym.make("urdf-env-v0", dt=0.01, robots=robots, render=render)
-    env.reset(pos=np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
+    ob = env.reset(pos=np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
 
-    # Joint limits and plotting setup
-    joint_limits = [(-2.8973, 2.8973), (-1.7628, 1.7628), (-2.8973, 2.8973)]
-    num_steps_per_joint = 100
-    x_positions = [[], [], []]
-    y_positions = [[], [], []]
-    z_positions = [[], [], []]
+    #MULTIPLE JOINTS
+    # joint_limits = [(-2.8973, 2.8973), (-1.7628, 1.7628), (-2.8973, 2.8973)]
+    # coordinates = [[], [], []]  # Separate list for each joint
+    # dot_colors = [
+    # [1.0, 0.0, 0.0],  # Red for the first joint
+    # [0.0, 1.0, 0.0],  # Green for the second joint
+    # [0.0, 0.0, 1.0]   # Blue for the third joint
+    # ]
 
-    # Define a small radius for the dots
+    joint_limits = [(-2.8973, 2.8973)]  # Only the first joint's limits
+    coordinates = [[]]  # List for the first joint only
+    dot_colors = [[1.0, 0.0, 0.0]]  # Red for the first joint
     dot_radius = 0.02
-    dot_colors = [[1, 0, 0, 1], [0, 1, 0, 1], [0, 0, 1, 1]]  # Red, Green, Blue
+
+    dot_radius = 0.02
+
 
     for joint_index, (lower_limit, upper_limit) in enumerate(joint_limits):
-        for step in np.linspace(lower_limit, upper_limit, num_steps_per_joint):
+        for angle in np.linspace(lower_limit, upper_limit, n_steps):
             joint_angles = [0, 0, 0]
-            joint_angles[joint_index] = step
+            joint_angles[joint_index] = angle
 
+            # Simulate the action in the environment
             action = np.zeros(env.n())
-            action[3:6] = joint_angles
-            env.step(action)
+            action[joint_index+2] = joint_angles[joint_index]
+            ob, *_ = env.step(action)
 
-            xyz = Kinematics3joints(joint_angles).forward_kinematics()
-            x_positions[joint_index].append(xyz[0])
-            y_positions[joint_index].append(xyz[1])
-            z_positions[joint_index].append(xyz[2])
-            print(z_positions[joint_index])
+            # Calculate the forward kinematics
+            transformation_matrix = Kinematics3joints(joint_angles).forward_kinematics()
+            x, y, z = transformation_matrix[0][3], transformation_matrix[1][3], transformation_matrix[2][3]
+            print(x)
+            xyz = [x, y, z]
+            coordinates[joint_index].append(xyz)
 
-            visual_shape_id = p.createVisualShape(shapeType=p.GEOM_SPHERE, radius=dot_radius, rgbaColor=dot_colors[joint_index])
-            p.createMultiBody(baseMass=0, baseVisualShapeIndex=visual_shape_id, basePosition=xyz)
+            # Store the coordinates
+            coordinates[joint_index].append([x, y, z])
+
+            # Visualize the point in the simulation
+            # rgba_color = dot_colors[joint_index] + [1]  # Adding alpha value for color
+            # visual_shape_id = p.createVisualShape(shapeType=p.GEOM_SPHERE, radius=dot_radius, rgbaColor=rgba_color)
+            # p.createMultiBody(baseMass=0, baseVisualShapeIndex=visual_shape_id, basePosition=xyz)
+
 
     env.close()
-
-    # Plotting
+    
+   # Plotting the trajectories
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    colors = ['r', 'g', 'b']
-    for i in range(3):
-        ax.scatter(x_positions[i], y_positions[i], z_positions[i], c=colors[i], label=f'Joint {i+1}')
-    ax.set_xlabel('X Position')
-    ax.set_ylabel('Y Position')
-    ax.set_zlabel('Z Position')
-    plt.title('3D End-Effector Positions for Joint Movements')
-    plt.legend()
+    colors = ['r', 'g', 'b']  # Colors for each joint
+
+    for i in range(len(coordinates)):
+        joint_coordinates = np.array(coordinates[i])
+        ax.plot(joint_coordinates[:, 0], joint_coordinates[:, 1], joint_coordinates[:, 2], color=colors[i], label=f'Joint {i+1}')
+
+    ax.set_xlabel('X axis')
+    ax.set_ylabel('Y axis')
+    ax.set_zlabel('Z axis')
+    ax.legend()
     plt.show()
 
+    return coordinates
+
 if __name__ == "__main__":
+    show_warnings = False
+    warning_flag = "default" if show_warnings else "ignore"
     with warnings.catch_warnings():
-        warnings.filterwarnings("ignore")
-        run_albert(render=True)
+        warnings.filterwarnings(warning_flag)
+        coordinates = run_albert(render=True)
