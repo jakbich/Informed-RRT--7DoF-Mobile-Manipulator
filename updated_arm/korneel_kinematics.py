1719d2351bc910635e673de6a04c1762d206e923
ksomers <ksomers@gitlab.tudelft.nl>
2023-12-14 16:30:39 +0100

    eureka

--- updated_arm/korneel_kinematics.py
+++ updated_arm/korneel_kinematics.py
@@ -1,50 +1,40 @@
+from sympy import symbols, cos, sin, pi, Matrix, eye, lambdify
 import numpy as np
-from sympy import symbols, Matrix, eye, sin, cos, pi, lambdify
-# from numba import jit
+from numba import jit
 
 class Kinematics:
     def __init__(self):
-        # Initialize joint angle symbols
+        # Define joint angles as symbols
         self.q1, self.q2, self.q3, self.q4, self.q5, self.q6, self.q7 = symbols('theta_1 theta_2 theta_3 theta_4 theta_5 theta_6 theta_7')
         self.joint_angles = [self.q1, self.q2, self.q3, self.q4, self.q5, self.q6, self.q7]
 
-        # Define DH parameters and construct direct kinematics
-        self.DK = self.direct_kinematics()
-
-        # Flatten to a column vector
-        A = self.DK[0:3, 0:4]
-        A = A.transpose().reshape(12, 1)
-
-        # Compute Jacobian symbolically
-        J = A.jacobian(Matrix(self.joint_angles))
-
-        # Convert to numerical functions using lambdify and jit for speed
-        self.A_lamb = lambdify(self.joint_angles, A, 'numpy')
-        self.J_lamb = lambdify(self.joint_angles, J, 'numpy')
-
-    def direct_kinematics(self):
-        # DH parameters for the Panda robot
-        dh_craig = [
-            {'a':  0,      'd': 0.333, 'alpha':  0},
-            {'a':  0,      'd': 0,     'alpha': -pi/2},
-            {'a':  0,      'd': 0.316, 'alpha':  pi/2},
-            {'a':  0.0825, 'd': 0,     'alpha':  pi/2},
-            {'a': -0.0825, 'd': 0.384, 'alpha': -pi/2},
-            {'a':  0,      'd': 0,     'alpha':  pi/2},
-            {'a':  0.088,  'd': 0.107, 'alpha':  pi/2},
+        # Craig's DH parameters
+        self.dh_craig = [
+            {'a':  0,      'd': 0.333, 'alpha':  0    },
+            {'a':  0,      'd': 0,     'alpha': -pi/2 },
+            {'a':  0,      'd': 0.316, 'alpha':  pi/2 },
+            {'a':  0.0825, 'd': 0,     'alpha':  pi/2 },
+            {'a': -0.0825, 'd': 0.384, 'alpha': -pi/2 },
+            {'a':  0,      'd': 0,     'alpha':  pi/2 },
+            {'a':  0.088,  'd': 0.107, 'alpha':  pi/2 },
         ]
 
+        self.speed_limits = [
+                                (-2.8973, 2.8973),
+                                (-1.7628, 1.7628),
+                                (-2.8973, 2.8973),
+                                (-3.0718, -0.0698),
+                                (-2.8973, 2.8973),
+                                (-0.0175, 3.7525),
+                                (-2.8973, 2.8973)
+                            ]
+
+        # Compute direct kinematics matrix
         DK = eye(4)
-
-        for p, q in zip(reversed(dh_craig), reversed(self.joint_angles)):
-            d = p['d']
-            a = p['a']
-            alpha = p['alpha']
-
-            ca = cos(alpha)
-            sa = sin(alpha)
-            cq = cos(q)
-            sq = sin(q)
+        for p, q in zip(reversed(self.dh_craig), reversed(self.joint_angles)):
+            d, a, alpha = p['d'], p['a'], p['alpha']
+            ca, sa = cos(alpha), sin(alpha)
+            cq, sq = cos(q), sin(q)
 
             transform = Matrix([
                 [cq, -sq, 0, a],
@@ -54,28 +44,23 @@ class Kinematics:
             ])
 
             DK = transform @ DK
-        
-        return DK
 
-    def get_matrices(self, joint_values):
-        A = self.A_lamb(*joint_values)
-        J = self.J_lamb(*joint_values)
-        return A, J
+        # Compute A matrix and Jacobian
+        A = DK[0:3, 0:4].transpose().reshape(12, 1)
+        J = A.jacobian(Matrix(self.joint_angles))
 
+        # Lambdify the matrices for faster computation
+        self.A_lamb = lambdify(self.joint_angles, A, 'numpy')
+        self.J_lamb = lambdify(self.joint_angles, J, 'numpy')
+    
+    def FK(self, joint_positions, xyz=False):
+        q = joint_positions
+        A = self.A_lamb(*q)
+        return A.flatten()[-3:], A
+    
 
-# Main function
-if __name__ == "__main__":
-    robot = Kinematics()
-
-    # Example joint angles (in radians)
-    joint_angles = [0.0, 0.0, 0.0, -1.5708, 0.0, 1.8675, 0.0]
-
-    A_matrix, J_matrix = robot.get_matrices(joint_angles)
-    position = A_matrix.flat[-3:]
-    # print("A Matrix:\n", A_matrix)
-    print("Position:", position)
-
-    print("Jacobian Matrix:\n", J_matrix)
-
-    print("A Matrix Shape:", A_matrix.shape)
-    print("Jacobian Matrix Shape:", J_matrix.shape)
+    def jacobian(self, joint_positions):
+        q = joint_positions
+        J = self.J_lamb(q[0], q[1], q[2], q[3], q[4], q[5], q[6])
+        J = J/np.linalg.norm(J)
+        return J
--- updated_arm/korneel_operate.py
+++ updated_arm/korneel_operate.py
@@ -4,7 +4,6 @@ import pybullet as p
 from urdfenvs.robots.generic_urdf import GenericUrdfReacher
 from urdfenvs.urdf_common.urdf_env import UrdfEnv
 from korneel_control import ArmControl
-from korneel_kinematics import Kinematics
 
 # Import or define ArmControl and Kinematics as needed
 
@@ -34,24 +33,35 @@ def run_panda(n_steps=1000, render=False, goal=True, obstacles=False):
     # Add a visual marker at the target position
     # target_position = np.array([0.550655275233520, -1.68862385257615E-17, 0.65])  # Adjust as needed
     target_position = np.array([0.31940916,  -0.21587151,  1.09758974 ])
+    # target_position = np.array([0.58193844, 0.5       ,0.654902])
     visual_shape_id = p.createVisualShape(shapeType=p.GEOM_SPHERE, radius=0.07, rgbaColor=[1, 0, 0, 1])
     p.createMultiBody(baseMass=0, baseVisualShapeIndex=visual_shape_id, basePosition=target_position)
 
     history = []
-
-    kinematics = Kinematics()
-    arm_control = ArmControl(kinematics)
+    xyz_history = []  # To store XYZ values
+    error_hisotry = []
+    arm_control = ArmControl()
 
     for i in range(n_steps):
         # Get the current joint angles
-        current_joint_angles = np.array(ob['robot_0']['joint_state']['position'][:7]).reshape(7, 1)
+        current_joint_angles = np.array(ob['robot_0']['joint_state']['position'][:7])
 
         # Calculate joint space control action using ArmControl
-        joint_vel = arm_control.PID(target_position, current_joint_angles, endpoint_orientation=False)
+        joint_space_action, error = arm_control.control_action(current_joint_angles, target_position)
+
+        # Construct the full control action array
+        control_action = np.zeros(env.n())
+        control_action[:7] = joint_space_action # Assuming the first 7 elements are for joint control
+        # print(control_action[:7])
 
         # Step the environment with the computed control action
-        ob, *_ = env.step(joint_vel)
+        ob, *_ = env.step(control_action)
 
+        # Compute the current XYZ position of the end effector
+        # xyz, _ = Kinematics().FK(current_joint_angles)
+        # p.createMultiBody(baseMass=0, baseVisualShapeIndex=visual_shape_id, basePosition=xyz)
+        # xyz_history.append(xyz)  # Save XYZ values
+        # error_hisotry.append(np.linalg.norm(error))
         history.append(ob)
 
     env.close()
@@ -61,7 +71,7 @@ def run_panda(n_steps=1000, render=False, goal=True, obstacles=False):
     from mpl_toolkits.mplot3d import Axes3D
 
     xyz_history = np.array(xyz_history)
-    print('xyz_history', xyz_history[500])
+    # print('xyz_history', xyz_history[500])
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot(xyz_history[:, 0], xyz_history[:, 1], xyz_history[:, 2])
@@ -69,6 +79,15 @@ def run_panda(n_steps=1000, render=False, goal=True, obstacles=False):
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     ax.set_title('Endpoint Position (XYZ) Over Time')
+    plt.show()
+    
+    # Plot the error over time
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(error_hisotry)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Error')
+    ax.set_title('Error Over Time')
     plt.show()
 
     return history
